f4380cd97243a81135fca215e6bd14155db1a34b
Glaucio Ricardo <glaucio.ricardo.jun@accenture.com>
2020-06-04 23:36:56 -0300

    feature: ajustando nome

--- src/core/database/seeds/create_default_items.ts
+++ src/core/database/seeds/create_default_items.ts
@@ -5,7 +5,7 @@ export async function seed(knex: Knex) {
   return await knex('items').insert([
     { title: 'Lâmpadas', image: 'lampadas.svg' },
     { title: 'Pilhas e Baterias', image: 'baterias.svg' },
-    { title: 'Papéis e Papelão', image: 'papeis-papelão.svg' },
+    { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
     { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
     { title: 'Residuos Orgânicos', image: 'organicos.svg' },
     { title: 'Oléo de Cozinha', image: 'oleo.svg' },
